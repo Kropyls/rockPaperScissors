d82c4e2c97cb6c4fbbb5eca93e8b4e206765ab57
Kropyls <Kropyls@gmail.com>
2022-03-07 19:32:23 -0800

    initial commit

---
+++ skeleton-script.js
@@ -0,0 +1,25 @@
+/*
+play round takes player selection and computer selection
+turn player selection to lower case
+check to make sure player selection valid
+pick one of the 3 for computer selection 
+
+*/
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection.toLowerCase();
+  if ((playerSelection == "rock") || (playerSelection == "scissors") || (playerSelection == "paper"))
+    {
+      continue;
+    }
+  else
+  {
+    return "please enter a valid choice";
+  }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
