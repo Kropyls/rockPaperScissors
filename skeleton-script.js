25350c67037f24df6e846455d328cbeab75b0b0c
Kropyls <Kropyls@gmail.com>
2022-03-10 15:13:16 -0800

    got it working. cleaned up playRound function, added end_message function

--- skeleton-script.js
+++ skeleton-script.js
@@ -6,7 +6,8 @@ turn player selection to lower case
 check to make sure player selection valid
 pick one of the 3 for computer selection 
 checks for tie
-switch case to evaulate winner
+evaluate all win cases for player
+else return loss
 */
 
 function computerPlay(){
@@ -15,14 +16,18 @@ function computerPlay(){
   return select;
 }
 
-function playRound(playerSelection, computerSelection) {
-  let win_msg = "You win! "
-  let lose_msg = "You lose! "
+function end_message(result,winpick,losepick){
+  return `You ${result}! ${winpick} beats ${losepick}!`;
+}
+
+function playRound(playerSelection, computerSelection){
   playerSelection = playerSelection.toLowerCase();
+  //evaluates player input for validity
   if (!(playerSelection == "rock") || (playerSelection == "scissors") || (playerSelection == "paper"))
     {
-    return "please enter a valid choice";
+    return "please enter a valid choice; choices are either rock, paper, or scissors";
     }
+  //checks for tie case
   if (playerSelection == computerSelection){
     return `Tie! You both picked ${playerSelection}`;
   }
@@ -32,12 +37,16 @@ function playRound(playerSelection, computerSelection) {
     ((playerSelection=="scissors")&&(computerSelection=="paper")) ||
     ((playerSelection=="paper")&&(computerSelection=="rock"))
     ){
-      return win_msg;
+      return end_message("win",playerSelection,computerSelection);
     }
-
+  //if we've checked for a tie and we'be checked for a win;
+  //it's safe to say the player lost!
+  else {
+    return end_message("lose",computerSelection,playerSelection);
+  }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+let playerSelection = "rock";
+let computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
