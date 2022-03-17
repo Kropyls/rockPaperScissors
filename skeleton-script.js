1b7125af8d1d8979f7cb246ab36de1a543fe1fcf
Kropyls <Kropyls@gmail.com>
2022-03-16 19:45:11 -0700

    discovered the same bug is still present. Added insite as to why in the comments and better console logging to catch the issue.

--- skeleton-script.js
+++ skeleton-script.js
@@ -20,6 +20,34 @@ end loop
 return winner
 */
 
+/*
+This is an accounting for an issue that showed up one night
+I cannot reproduce it, but I will leave this here in case it 
+show up again so I can work to isolate what might be causing
+it
+
+bug showed up with the following play:
+R player - com      = score
+
+W rock   - scissors = p1c0
+T rock   - rock     = p1c0
+T rock   - rock     = p1c0
+L rock   - paper    = p1c1
+T rock   - rock     = p1c1
+L rock   - paper    = p1c2
+L rock   - paper    = p1c3
+W rock   - scissors = p2c3
+L rock    - paper   = p2c4
+
+reported score: P2C4
+
+UPDATE: This bug occurs when the counter value i, resets itself to 0.
+I have no idea why it does this as of now but I have caught a log of it
+happening. So far I have not been able to steadily reproduce the problem.
+
+I'm going to commit this as is so I have a document of the issue
+*/
+
 function computerPlay(){
   let choices = ['rock','paper','scissors'];
   let select = choices[Math.floor(Math.random()*choices.length)];
@@ -61,6 +89,10 @@ function game(){
   let playerScore = 0;
   let computerScore = 0;
 
+  //temp variables for T/S
+  let totalRndPlayed = 0;
+  let currentRnd = 0;
+
   for (let i = 0; i < 5; i++) {
     let playerSelection = prompt('pick an option: rock paper or scissors')
     //evaluates player input for validity
@@ -75,22 +107,29 @@ function game(){
       alert("please enter a valid choice: rock, paper, or scissors");
       i=-1; continue;
       }
+
+    totalRndPlayed++;
+    currentRnd++;
     
     //now that player input is good, we can proceed with playing the round
-    let computerSelection = computerPlay();
+    let computerSelection = "scissors";//computerPlay();
     let result = playRound(playerSelection, computerSelection);
     switch(result){
       case 0:
         console.log(`Tie! You both picked ${playerSelection}! Re-doing round...`);
         i=i-1; 
+        currentRnd = currentRnd - 1;
+        console.log(`\nTotal Rounds: ${totalRndPlayed}\nCurrent Round: ${currentRnd}\ni = ${i}\n`);
         continue;
       case 1:
         console.log(end_message("win",playerSelection,computerSelection))
         playerScore++;
+        console.log(`\nTotal Rounds: ${totalRndPlayed}\nCurrent Round: ${currentRnd}\ni = ${i}\n`);
         break;
       case 2:
         console.log(end_message("lose",computerSelection,playerSelection))
         computerScore++;
+        console.log(`\nTotal Rounds: ${totalRndPlayed}\nCurrent Round: ${currentRnd}\ni = ${i}\n`);
         break;
       default:
         throw `playRound returned a bad number -- the code should never get here: player input = ${playerSelection}, computer input = ${computerSelection}`;
